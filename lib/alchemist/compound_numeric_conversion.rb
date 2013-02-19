03a5f9fb5dfe1a9ae19fbb611c13a986d2c02218
halogenandtoast <halogenandtoast@gmail.com>
2013-02-18 22:42:45 -0500

    Cleanup CompoundNumericConversion.

--- lib/alchemist/compound_numeric_conversion.rb
+++ lib/alchemist/compound_numeric_conversion.rb
@@ -21,18 +21,32 @@ module Alchemist
 
     # TODO: minify this
     def consolidate
+      compress_units
+      set_coefficients
+    end
+
+    def compress_units
       @numerators.each_with_index do |numerator, n|
         @denominators.each_with_index do |denominator, d|
-          next if numerator.is_a?(Numeric)
-          next if denominator.is_a?(Numeric)
-          if (Alchemist.measurement_for(numerator.unit_name) & Alchemist.measurement_for(denominator.unit_name)).length > 0
-            value = numerator / denominator
-            @numerators.delete_at(n)
-            @denominators.delete_at(d)
-            @coefficient *= value
-          end
+          remove_excess_units(numerator, n, denominator, d)
         end
       end
+    end
+
+    def remove_excess_units numerator, n, denominator, d
+      if should_remove_units? numerator, denominator
+        @numerators.delete_at(n)
+        @denominators.delete_at(d)
+        @coefficient *= (numerator / denominator)
+      end
+    end
+
+    def should_remove_units? numerator, denominator
+      return false if numerator.is_a?(Numeric) || denominator.is_a?(Numeric)
+      (Alchemist.measurement_for(numerator.unit_name) & Alchemist.measurement_for(denominator.unit_name)).length > 0
+    end
+
+    def set_coefficients
       if @denominators.length == 0 && @numerators.length == 1
         @numerators[0] * @coefficient
       elsif @denominators.length == 0 && @numerators.length == 0
