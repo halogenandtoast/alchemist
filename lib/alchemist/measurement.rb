75be73f0390a2c0eafd3fc45ba4d03ae51dc4cb5
halogenandtoast <halogenandtoast@gmail.com>
2013-07-21 13:23:26 -0400

    Simplify raising dimension.

--- lib/alchemist/measurement.rb
+++ lib/alchemist/measurement.rb
@@ -41,13 +41,10 @@ module Alchemist
     end
 
     def * multiplicand
-      if wrap = check_for_conversion_wrap(:*, multiplicand)
-        return wrap.value
-      end
       if multiplicand.is_a?(Numeric)
         Measurement.new(value * multiplicand, unit_name, exponent)
       else
-        raise Exception, "Incompatible Types"
+        try_raising_dimension(multiplicand)
       end
     end
 
@@ -70,6 +67,10 @@ module Alchemist
 
     def <=>(other)
       (self.to_f * exponent).to_f <=> other.to(unit_name).to_f
+    end
+
+    def types
+      Alchemist.measurement_for(unit_name)
     end
 
     def shared_types other_unit_name
@@ -96,38 +97,18 @@ module Alchemist
       Alchemist.conversion_table[unit_type][unit_name]
     end
 
-    def types
-      Alchemist.measurement_for(unit_name)
-    end
-
     def has_shared_types? other_unit_name
       shared_types(other_unit_name).length > 0
     end
 
-    class ConversionWrap < Struct.new(:value)
-    end
-
-    def check_for_conversion_wrap(unit_name, arg)
-      if can_perform_conversion?(unit_name, arg)
-        wrap = check_operator_conversion(unit_name, arg)
-        if wrap.is_a?(ConversionWrap)
-          return wrap
+    def try_raising_dimension(measurement)
+      valid_types = shared_types(measurement.unit_name)
+      Alchemist.operator_actions[:*].each do |s1, s2, new_type|
+        if (valid_types & [s1, s2]).any?
+          return Alchemist.measurement(value * measurement.to_f, new_type)
         end
       end
-    end
-
-    def can_perform_conversion? unit_name, arg
-      arg.is_a?(Measurement) && Alchemist.operator_actions[unit_name]
-    end
-
-    def check_operator_conversion unit_name, arg
-      t1 = Alchemist.measurement_for(self.unit_name)[0]
-      t2 = Alchemist.measurement_for(arg.unit_name)[0]
-      Alchemist.operator_actions[unit_name].each do |s1, s2, new_type|
-        if t1 == s1 && t2 == s2
-          return ConversionWrap.new((value * arg.to_f).send(new_type))
-        end
-      end
+      incompatible_types
     end
 
     def incompatible_types
