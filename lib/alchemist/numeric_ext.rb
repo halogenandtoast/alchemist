f9f5532dfb635b7437a933e060c142bf2f1c005a
halogenandtoast <halogenandtoast@gmail.com>
2013-07-21 14:48:24 -0400

    Help out with method_missing speed.

--- lib/alchemist/numeric_ext.rb
+++ lib/alchemist/numeric_ext.rb
@@ -1,6 +1,10 @@
 module Alchemist
   module Conversion
+    NON_ALCHEMIST_METHODS = [:to_s, :to_str, :to_a, :to_ary]
+
     def method_missing unit_name, *args, &block
+      return super if NON_ALCHEMIST_METHODS.include?(unit_name)
+
       exponent, unit_name = Alchemist.parse_prefix(unit_name)
       if Alchemist.has_measurement?(unit_name)
         Alchemist.measurement self, unit_name, exponent
