add97c666103092e430385abb8e7cd75e071f7ba
halogenandtoast <halogenandtoast@gmail.com>
2014-02-01 10:58:01 -0500

    Cleanup ConversionTable spec

--- spec/conversion_table_spec.rb
+++ spec/conversion_table_spec.rb
@@ -2,27 +2,17 @@ require 'spec_helper'
 
 module Alchemist
   describe ConversionTable do
-    after(:each) do
-      load_file default_file
+
+    it "loads a properly formed file" do
+      conversion_table = ConversionTable.new
+      expect(conversion_table.load_all(good_file)).not_to be_nil
     end
 
-    it "should use the new file" do
-      load_file good_file
-      expect(conversion_table[:volume]).to eq({ litre: 1.0, swallow: 0.006, pint: 0.5506105 })
+    it "fails to load a improperly formed file" do
+      conversion_table = ConversionTable.new
+      expect(conversion_table.load_all(bad_file)).to be_nil
     end
 
-    it "should use the defaults when it fails to load" do
-      load_file bad_file
-      expect(5280.feet).to eq(1.mile.to.feet)
-    end
-
-    def conversion_table
-      Alchemist.library.conversion_table
-    end
-
-    def load_file file
-      Alchemist.library.load_conversion_table file
-    end
 
     def good_file
       File.join(File.dirname(__FILE__), "fixtures", "good_test.yml")
@@ -30,10 +20,6 @@ module Alchemist
 
     def bad_file
       File.join(File.dirname(__FILE__), "fixtures", "bad_test.yml")
-    end
-
-    def default_file
-      Configuration::DEFAULT_UNITS_FILE
     end
   end
 end
