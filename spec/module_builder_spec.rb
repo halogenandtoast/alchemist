06061b67bb134a9a5129e31fb5ae4b1a24c9be82
Chris Salzberg <csalzberg@degica.com>
2017-08-09 10:36:39 +0900

    Refactor using Module subclass

--- spec/module_builder_spec.rb
+++ spec/module_builder_spec.rb
@@ -5,9 +5,8 @@ module Alchemist
     it "allows methods to be added to the build module" do
       allow(Alchemist).to receive(:library).and_return(library_double)
       builder = ModuleBuilder.new(:test)
-      test_module = builder.build
-      test_module.define_unit_method([:wombat])
-      expect(test_module.instance_methods).to include(:wombat)
+      builder.define_unit_method([:wombat])
+      expect(builder.instance_methods).to include(:wombat)
     end
 
     def library_double
